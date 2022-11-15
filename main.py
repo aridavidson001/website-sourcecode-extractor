7611bf8fe192a1a19a21cc6895ab1ca9b7f28c99
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 13:32:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #Python program to scrape website
 #and save quotes from website
 import requests
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import streamlit as st
 
 try:
   source = requests.get(st.text_input("Insert URL here"))
   source.raise_for_status()
-  st.write(source)
-#  soup = BeautifulSoup(source.text,'html.parser')
- # st.write(soup)
+  #st.write(source)
+  soup = BeautifulSoup(source.text,'html.parser')
+  st.write(soup)
   
 except Exception as e:
   st.write(e)
