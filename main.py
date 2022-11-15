680dfa01d1a5559e5d265f9de9628b4e068a376e
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 13:31:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-
+#Python program to scrape website
+#and save quotes from website
 import requests
 #from bs4 import BeautifulSoup
 import streamlit as st
 
-source_url = st.text_input("Insert URL Here")
-submit = st.button("submit") 
-with submit:
-  try:
-     source = requests.get('https://slither.io')
-     source.raise_for_status()
-     st.write(source)
+try:
+  source = requests.get(st.text_input("Insert URL here"))
+  source.raise_for_status()
+  st.write(source)
 #  soup = BeautifulSoup(source.text,'html.parser')
  # st.write(soup)
   
-  except Exception as e:
-    st.write(e)
+except Exception as e:
+  st.write(e)
