bffd9cfd380fdf90d8a82d432104a7d3e3e6aaa1
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 17:58:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ import streamlit.components.v1 as components
 import requests
 from bs4 import BeautifulSoup
 
-st.header("test html import")
 
-HtmlFile = open("test.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
-components.html(source_code)
-
-#more
 import streamlit as st
 url = st.text_input("Insert URL here")
-try:
-  source = requests.get(url)
-  source.raise_for_status()
-  st.write(source)
-  soup = BeautifulSoup(source.text,'html.parser')
-  print(soup)
+with url:
+  try:
+    source = requests.get(url)
+    source.raise_for_status()
+    st.write(source)
+    soup = BeautifulSoup(source.text,'html.parser')
+    components.html(soup)
+    print(soup)
   
-except Exception as e:
-  st.write(e)
+  except Exception as e:
+    st.write(e)
 
 
