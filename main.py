800378c97ea5b8923fc15186187a62248313815c
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 17:56:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# >>> import plotly.express as px
-# >>> fig = px.box(range(10))
-# >>> fig.write_html('test.html')
+import requests
+from bs4 import BeautifulSoup
 
 st.header("test html import")
 
@@ -11,3 +10,18 @@ HtmlFile = open("test.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
 components.html(source_code)
+
+#more
+import streamlit as st
+url = st.text_input("Insert URL here")
+try:
+  source = requests.get(url)
+  source.raise_for_status()
+  st.write(source)
+  soup = BeautifulSoup(source.text,'html.parser')
+  print(soup)
+  
+except Exception as e:
+  st.write(e)
+
+
