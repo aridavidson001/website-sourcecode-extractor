90e32009646425d28e116080a979fe25dfac8d64
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 13:37:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
-
+url = st.text_input("Insert URL here")
 try:
-  source = requests.get(st.text_input("Insert URL here"))
+  source = requests.get(url)
   source.raise_for_status()
   st.write(source)
   soup = BeautifulSoup(source.text,'html.parser')
