8dfd21841320830ad631cc5a10a1e804048267d1
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 13:03:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import requests
 import streamlit as st
 
 source_url = st.text_input("Insert URL Here")
-try:
-  source = requests.get(source_url)
-  source.raise_for_status()
-  st.write(source)
+with source_url:
+  try:
+    source = requests.get(source_url)
+    source.raise_for_status()
+    st.write(source)
 #  soup = BeautifulSoup(source.text,'html.parser')
  # st.write(soup)
   
-except Exception as e:
-  st.write(e)
+  except Exception as e:
+    st.write(e)
