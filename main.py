3e87b2ec672d3e6f119e4b74b91f72bd0f15a62a
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 13:24:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import requests
 import streamlit as st
 
 source_url = st.text_input("Insert URL Here")
-submit = st.button("submit")
-with submit:
-  try:
-    source = requests.get(source_url)
-    source.raise_for_status()
-    st.write(source)
+submit = st.button("submit") 
+  with submit:
+    try:
+      source = requests.get(source_url)
+      source.raise_for_status()
+      st.write(source)
 #  soup = BeautifulSoup(source.text,'html.parser')
  # st.write(soup)
   
