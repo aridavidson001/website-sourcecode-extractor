c70d5ffb219d3a6284f1f41ca3ea2a8b5343df69
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-15 18:27:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ try:
   st.write(source)
   soup = BeautifulSoup(source.text,'html.parser')
   print(soup)
-  soup.encode()
+ # soup.encode()
  # HtmlFile = encode(soup, 'r', encoding='utf-8')
-  
+  soup.encode(encoding="UTF-8")
+
   st.write('soup')
   components.html(source)
   
